6f0d1cab28d5293c7674c03fd46542cbf22835fd
Anuj chaturvedi <89034978+Anuj6@users.noreply.github.com>
2024-01-30 22:16:04 +0530

    Time: 26 ms (94.33%), Space: 29.9 MB (18.95%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -13,9 +13,36 @@ public:
 //         return ans;
         
 // 2. with sorting 
-        sort(nums.begin(), nums.end());
-        // product can be positive, zero, negative == max(smallest 2 * largest no.,  largest 3)
-        int n = size(nums);
-        return max(nums[0]*nums[1]*nums[n-1], nums[n-1]*nums[n-2]*nums[n-3]);
+        // sort(nums.begin(), nums.end());
+        // // product can be positive, zero, negative == max(smallest 2 * largest no.,  largest 3)
+        // int n = size(nums);
+        // return max(nums[0]*nums[1]*nums[n-1], nums[n-1]*nums[n-2]*nums[n-3]);
+        
+// 3. one pass optimal solution 
+        int max1 = INT_MIN; int max2 = INT_MIN; int max3 = INT_MIN;
+        int min1 = INT_MAX; int min2 = INT_MAX;
+        
+        for(int i = 0; i<size(nums); i++){
+            if(nums[i] > max1){
+                max3 = max2;
+                max2 = max1;
+                max1 = nums[i];
+            }else if(nums[i] > max2){
+                max3 = max2;
+                max2 = nums[i];
+            }else if(nums[i] > max3){
+                max3 = nums[i];
+            }
+            
+            if(nums[i] < min1){
+                min2 = min1;
+                min1 = nums[i];
+            }else if(nums[i]<min2){
+                min2 = nums[i];
+            }
+            
+        }
+        
+        return max(min1*min2*max1, max1*max2*max3);
     }
 };
