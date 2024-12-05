a6d02bcbfed8b13fa22bf9b95096d8e18b35489a
Anuj chaturvedi <89034978+Anuj6@users.noreply.github.com>
2024-12-06 00:50:12 +0530

    Time: 4 ms (64.85%), Space: 14.9 MB (13.45%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,12 +4,24 @@ public:
         unordered_map<int, int> mp;
         
         // map me bharo element and key == index
+//         int n = size(nums);
+//         for(int i = 0; i<n; i++){
+//             int need_more =  target - nums[i];
+            
+//             if(mp.find(need_more) != mp.end()){
+//                  return {mp[need_more], i};
+//             }
+//             mp[nums[i]] = i;
+//         }
+//         return {-1, -1};
+        
+        // sum == target 
         int n = size(nums);
         for(int i = 0; i<n; i++){
-            int need_more =  target - nums[i];
+            int need_more = target - nums[i];
             
             if(mp.find(need_more) != mp.end()){
-                 return {mp[need_more], i};
+                return {mp[need_more], i};
             }
             mp[nums[i]] = i;
         }
